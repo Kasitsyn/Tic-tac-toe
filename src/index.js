af1fca1cc3d1b5a3a75736bfb8af3172fd25db87
kasitsyn <kasitsyn@gmail.com>
2023-02-06 18:18:24 +0700

    feat: rewrite Board with loops

--- src/index.js
+++ src/index.js
@@ -24,19 +24,13 @@ class Board extends React.Component {
     return (
       <div>
         <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+          {[0, 1, 2].map((square) => this.renderSquare(square))}
         </div>
         <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
+          {[3, 4, 5].map((square) => this.renderSquare(square))}
         </div>
         <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
+          {[6, 7, 8].map((square) => this.renderSquare(square))}
         </div>
       </div>
     );
